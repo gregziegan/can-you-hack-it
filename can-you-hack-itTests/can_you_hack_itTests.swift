940202b57478a23d4c86e2e4f10b81143a1e526b
Greg Ziegan <grz5@case.edu>
2014-11-10 17:09:25 -0500

    initial commit

---
+++ can-you-hack-itTests/can_you_hack_itTests.swift
@@ -0,0 +1,36 @@
+//
+//  can_you_hack_itTests.swift
+//  can-you-hack-itTests
+//
+//  Created by Gregory Ziegan on 11/10/14.
+//  Copyright (c) 2014 CWRU Undergrad. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class can_you_hack_itTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
